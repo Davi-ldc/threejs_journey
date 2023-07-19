cd1de6700df06394804598bc89b88fe34775088b
Davi-ldc <96698943+Davi-ldc@users.noreply.github.com>
2023-07-19 18:15:00 -0300

    aula 13

--- particulas/src/script.js
+++ particulas/src/script.js
@@ -5,6 +5,14 @@ import * as dat from 'lil-gui'
 THREE.ColorManagement.enabled = false
 
 /**
+ * particles
+ */
+// Geometry
+const tl = new THREE.TextureLoader()
+const map = tl.load('/textures/particles/2.png')
+
+
+/**
  * Base
  */
 // Debug
@@ -25,7 +33,7 @@ const textureLoader = new THREE.TextureLoader()
  * Test cube
  */
 // const cube = new THREE.Mesh(
-//     new THREE.BoxGeometry(1, 1, 1),
+//     new THREE.BoxGeometry(1, 1,1),
 //     new THREE.MeshBasicMaterial()
 // )
 // scene.add(cube)
@@ -34,13 +42,39 @@ const textureLoader = new THREE.TextureLoader()
  * particles
  */
 // Geometry
-const particlesGeometry = new THREE.SphereGeometry(1, 32, 32)
-const particlesmaterial = new THREE.PointsMaterial({size:0.02, sizeAttenuation:true})
-//sizeAttenuation é perspectiva 
 
-//points
+const count = 25000
+
+const positions = new Float32Array(count*3)//caunt é o numero d particulas e ta vezes 3 pq e xyz
+const colors = new Float32Array(count*3)//*3 pq é rgb
+for (let i = 1; i<count*3; i++){
+    positions[i] = (Math.random()-0.5) *10
+    colors[i] = Math.random()-0.01
+}
+
+
+
+
+const particlesGeometry = new THREE.BufferGeometry().setAttribute('position', new THREE.BufferAttribute(positions, 3))
+particlesGeometry.setAttribute('color',new THREE.BufferAttribute(colors,3))
+const particlesmaterial = new THREE.PointsMaterial({sizeAttenuation:true })
+// sizeAttenuation é perpectiva 
+
+particlesmaterial.vertexColors=true// pra usar as cores q eu criei
+
+// particlesmaterial.color = new THREE.Color(0xff88cc)
+particlesmaterial.alphaMap = map
+particlesmaterial.alphaTest = 0.001 //deixa o material mais transparente
+//particlesmaterial.depthTest = false //agora se vc tiver uma esfera atraz de um cubo a gpu so desenha os 2 idependente de quem ta na frente
+//particlesmaterial.depthWrite = false // mesma coisa do di cima mais se tiver z positivo não desenha nada que ta atraz dele
+particlesmaterial.blending = THREE.AdditiveBlending//quando 2 particulas tão no msm pixel, ele soma as cores dela (pode ser pesado)
+particlesmaterial.transparent = true
+particlesmaterial.size = 0.1
+
 const particles = new THREE.Points(particlesGeometry, particlesmaterial)
 scene.add(particles)
+
+
 
 /**
  * Sizes
@@ -55,6 +89,7 @@ window.addEventListener('resize', () =>
     // Update sizes
     sizes.width = window.innerWidth
     sizes.height = window.innerHeight
+
 
     // Update camera
     camera.aspect = sizes.width / sizes.height
@@ -71,7 +106,7 @@ window.addEventListener('resize', () =>
 // Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
 camera.position.z = 3
-camera.position.y = 0.5
+
 
 scene.add(camera)
 
@@ -97,6 +132,21 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+
+    // particles.rotation.y = elapsedTime*0.25
+
+
+    for(let i = 0; i < count; i++)
+    {
+        let i3 = i * 3
+
+        const x = particlesGeometry.attributes.position.array[i3]
+        particlesGeometry.attributes.position.array[i3 + 1] = Math.sin(elapsedTime + x)
+    }
+    particlesGeometry.attributes.position.needsUpdate = true
+
+
+
 
     // Update controls
     controls.update()
