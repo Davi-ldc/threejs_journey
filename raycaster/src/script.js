4ae6a438386f67c11bb555ebd31402ed3f0de84d
Davi-ldc <96698943+Davi-ldc@users.noreply.github.com>
2023-07-27 20:29:51 -0300

    raio

---
+++ raycaster/src/script.js
@@ -0,0 +1,163 @@
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import * as dat from 'lil-gui'
+
+THREE.ColorManagement.enabled = false
+
+
+/**
+ * Base
+ */
+// Debug
+const gui = new dat.GUI()
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+/**
+ * Objects
+ */
+const object1 = new THREE.Mesh(
+    new THREE.SphereGeometry(0.5, 16, 16),
+    new THREE.MeshBasicMaterial({ color: '#ff0000' })
+)
+object1.position.x = - 2
+
+const object2 = new THREE.Mesh(
+    new THREE.SphereGeometry(0.5, 16, 16),
+    new THREE.MeshBasicMaterial({ color: '#ff0000' })
+)
+
+const object3 = new THREE.Mesh(
+    new THREE.SphereGeometry(0.5, 16, 16),
+    new THREE.MeshBasicMaterial({ color: '#ff0000' })
+)
+object3.position.x = 2
+
+scene.add(object1, object2, object3)
+object1.updateMatrixWorld()
+object2.updateMatrixWorld()
+object3.updateMatrixWorld()
+
+/**
+ * raycaster
+ */
+const raycaster = new THREE.Raycaster()
+// const origen = new THREE.Vector3(-3,0,0)
+// const raydirection = new THREE.Vector3(10,0,0)
+
+// const intersec = raycaster.intersectObject(object1)
+// const intersecs = raycaster.intersectObjects([object1,object2,object3])
+/*
+ *distance: distancia entre o objeto e a origem da array 
+ *face: cara da geometria
+ *object: o objeto q atingiu a array 
+ *point: lugar da colisão
+ *uv: as cordenadas uv
+*/
+
+// raydirection.normalize()//o vector precisa medir 1
+// raycaster.set()
+
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+/**
+ * mouse
+ */
+let mouse = new THREE.Vector2()
+
+window.addEventListener('mousemove', (_event) =>{//valores vão de -1 a 1 
+    mouse.x = _event.clientX/sizes.height *2 -1
+    mouse.y = _event.clientY/sizes.width*2 -1
+})
+
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.z = 3
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.outputColorSpace = THREE.LinearSRGBColorSpace
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
+
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
+
+    // Update controls
+    controls.update()
+
+    //animate
+    object1.position.y=Math.sin(elapsedTime+1)*1.5
+    object2.position.y=Math.sin(elapsedTime+2)*1.5
+    object3.position.y=Math.sin(elapsedTime+3)*1.5
+
+    // const origen = new THREE.Vector3(-3,0,0)
+    // const raydirection = new THREE.Vector3(1,0,0)
+    // raydirection.normalize()
+    // raycaster.set(origen, raydirection)
+
+    // const objectToTest = [object1, object2, object3]
+    // const intersecs = raycaster.intersectObjects(objectToTest)
+
+    // for (const obj of objectToTest){
+    //     obj.material.color.set('#ff0000')
+    // }
+    // for (const objetoquetoconalinha of intersecs){
+    //     objetoquetoconalinha.object.material.color.set('#0000ff')
+    // }
+
+    
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
